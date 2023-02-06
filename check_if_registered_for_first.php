64f9b8a94e127841eccc769e4fc4faaf78c0b975
TatsuSolo <1730150@iub.edu.bd>
2023-02-06 17:26:03 +0600

    final

---
+++ check_if_registered_for_first.php
@@ -0,0 +1,28 @@
+<?php
+
+
+require_once 'db_function.php';
+$db = new DB_FUNCTIONS();
+
+$response = array("error" => FALSE);
+
+if (isset($_POST['barcode']) && $_POST['eventDetId']) {
+
+    $barcode = $_POST['barcode'];
+    $eventDetId = $_POST['eventDetId'];
+
+    $eventattendance_trs = $db->check_barcode($barcode, $eventDetId);
+
+    if ($eventattendance_trs) {
+        $response['error'] = FALSE;
+        $response['event']['eventId'] = $eventattendance_trs['ID'];
+    } else {
+        $response['error'] = TRUE;
+        $response['message'] = "Not scanned in Registration Event";
+    }
+    echo json_encode($response);
+} else {
+    $response['error'] = TRUE;
+    $response['error_msg'] = "Submit all fields";
+    echo json_encode($response);
+}
